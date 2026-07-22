9a60e5be50893f7127eaae13b382e9ecd0a75dec
toob4414-eng <229492237+toob4414-eng@users.noreply.github.com>
2026-07-21 20:38:21 -0400

    added basic pretty printing

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import requests, sys, json
 ip = sys.argv[1]
 
 lookup1 = requests.get("https://internetdb.shodan.io/"+ip).json()
-print(lookup1)
+print("ip:" + lookup1["ip"])
+print("open ports:")
+for port in lookup1["ports"]:
+    print(port)
+
+print("detected vulnerability:")
+for vuln in lookup1['vulns']:
+    print(vuln)
+
+print("tags:")
+for tag in lookup1["tags"]:
+    print(tag)
